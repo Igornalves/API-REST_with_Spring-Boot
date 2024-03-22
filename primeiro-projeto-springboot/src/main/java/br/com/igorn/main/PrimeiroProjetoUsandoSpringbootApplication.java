316971f1206fba52438289db53abf14b56edd558
igor_alves <igornalves08@gmail.com>
2024-03-22 13:46:22 -0300

    novo projeto spring boot

---
+++ primeiro-projeto-springboot/src/main/java/br/com/igorn/main/PrimeiroProjetoUsandoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorn.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoUsandoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoUsandoSpringbootApplication.class, args);
+	}
+
+}
---
+++ primeiro-projeto-springboot/src/test/java/br/com/igorn/main/PrimeiroProjetoUsandoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorn.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoUsandoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
